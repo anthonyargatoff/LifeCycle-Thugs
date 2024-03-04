260a9f6ffe1a85f9cf4baa52030a7584d2687ffd
RyanPybus <ryanpybus8596@gmail.com>
2024-03-04 12:26:58 -0800

    Redone tests in proper unittest format

--- SQLite/SQLiteTest.py
+++ SQLite/SQLiteTest.py
@@ -1,42 +1,38 @@
 # Import package
 import sqlite3;
+import unittest;
 
-# create connection and cursor
-con = sqlite3.connect("test.db");
-cursor = con.cursor();
 
-#no test for just select, but all tests use select.
+class test_SQLite(unittest.TestCase):
 
-# Test that pre-existing table still exists
-def TestPersistence(cursor):
-    result = cursor.execute("Select * From T1");
-    answer = result.fetchall()
-    if (answer == [(1, 'first'), (2, 'second')]):
-        print('Passed Persistence')
-    else: print('Failed Persistence');
+    # create connection and cursor
+    def setUp(self):
+        self.con = sqlite3.connect("test.db");
+        self.cursor = self.con.cursor();
 
-# Test that creating tables works
-def TestCreate(cursor):
-    cursor.execute('Create Table testTable(id Integer)');
-    result = cursor.execute("Select name From sqlite_master Where name='testTable'");
-    ans = result.fetchone()
-    if (ans == ('testTable',)):
-        print('Passed Create');
-        cursor.execute('Drop Table testTable');
-    else: print('Failed Create');
 
-# Test that adding rows works
-def TestInsert(cursor):
-    cursor.execute("Insert Into T1 values(5,'fifth'),(6,'sixth'),(7,'seventh')");
-    result = cursor.execute("Select * From T1 Where id > 3");
-    ans = result.fetchall()
-    if (ans == [(5, 'fifth'), (6, 'sixth'), (7, 'seventh')]):
-        print('Passed Insert');
-        cursor.execute("Delete From T1 Where id > 3");
-    else: print('Failed Insert');
+    #no test for just select, but all tests use select.
 
-TestPersistence(cursor);
-TestCreate(cursor);
-TestInsert(cursor);
+    # Test that pre-existing table still exists
+    def test_Persistence(self):
+        result = self.cursor.execute("Select * From T1");
+        self.assertEqual(result.fetchall(),[(1, 'first'), (2, 'second')]);
 
-con.close();
+    # Test that creating tables works
+    def test_Create(self):
+        self.cursor.execute('Create Table testTable(id Integer)');
+        result = self.cursor.execute("Select name From sqlite_master Where name='testTable'");
+        self.assertEqual(result.fetchone(), ('testTable',));
+        self.cursor.execute('Drop Table testTable');
+
+    # Test that adding rows works
+    def test_Insert(self):
+        self.cursor.execute("Insert Into T1 values(5,'fifth'),(6,'sixth'),(7,'seventh')");
+        result = self.cursor.execute("Select * From T1 Where id > 3");
+        self.assertEqual(result.fetchall(), [(5, 'fifth'), (6, 'sixth'), (7, 'seventh')]);
+        self.cursor.execute("Delete From T1 Where id > 3");
+
+    def tearDown(self):
+        self.con.close();
+
+unittest.main();
