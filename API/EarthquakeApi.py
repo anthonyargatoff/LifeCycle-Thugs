c958ce6764a9c0ece1b5416f0433d67863397fb5
Anthony <anthonyargatoff@gmail.com>
2024-03-11 11:15:24 -0700

    Created a method to create txt file - much smaller file size

--- API/EarthquakeApi.py
+++ API/EarthquakeApi.py
@@ -4,6 +4,7 @@ import calendar
 import os
 from datetime import date
 from datetime import timedelta
+import time
 
 class EarthquakeApi:
     """
@@ -18,7 +19,7 @@ class EarthquakeApi:
         """        
         self.url = url
 
-    def getAllData(self, startDate, endDate, fileLocation):
+    def getAllDataJSON(self, startDate, endDate, fileLocation):
         """
         Gets API data in a .json file format up to the specified time in a specific file location.
 
@@ -53,7 +54,7 @@ class EarthquakeApi:
                 with open("{}/{}_{:02d}.json".format(fileLocation, year, month), "w") as outfile:
                     json.dump(response, outfile, indent=4)
         
-    def getParsedData(self, startDate, endDate, fileLocation):
+    def getParsedDataJSON(self, startDate, endDate, fileLocation):
         """
         This method returns a condensed version of the API data, keeping most important data (time, location, strength, and a URL if more info is needed). If ran multiple times, it will replace the old json file with a newer version.
 
@@ -116,6 +117,64 @@ class EarthquakeApi:
             outfile.write("\n]")
             outfile.close()
 
+    def getParsedDataTXT (self, startDate, endDate, fileLocation):
+        """
+        Get data from the api from a start date to end date and outputs as a txt file. 
+
+        Args:
+            startDate (string): Date in format yyyy-mm
+            endDate (string): Date in format yyyy-mm
+            fileLocation (string): Specify relative file location (ex: API/earthquakeData)
+        """             
+        endYear, endMonth = endDate.split("-")
+        endYear = int(endYear)
+        endMonth = int(endMonth)
+        startYear, startMonth = startDate.split("-")
+        startYear = int(startYear)
+        startMonth = int(startMonth)
+
+        if os.path.exists("earthquakeJsonData/earthquakesParsed.txt"):
+            os.remove("earthquakeJsonData/earthquakesParsed.txt")
+
+        # Opens the json file for writing
+        with open("{}/earthquakesParsed.txt".format(fileLocation), "w") as outfile:
+
+            for year in range(startYear, endYear + 1):
+                for month in range(1, 13):
+                    if year == endYear and month > endMonth: # If the date is larger than the requested date, program stops
+                        break
+                    numberOfDays = calendar.monthrange(year, month)[1] # gets the number of days in the current month
+                    startDate = "{}-{:02d}-01".format(year, month)
+                    endDate = "{}-{:02d}-{}".format(year, month, numberOfDays)
+
+                    while 1:
+                        try:
+                            # Get requests for API
+                            response = requests.get("{}query?format=geojson&starttime={}&endtime={}".format(self.url, startDate, endDate)).json()
+                            print("{}query?format=geojson&starttime={}&endtime={}".format(self.url, startDate, endDate))
+                            
+                            # Parses the data we require
+                            for feature in response["features"]:
+                                properties = feature["properties"]
+                                event_time = properties["time"] # in milliseconds
+                                mag = properties["mag"]
+                                title = properties["title"]
+                                url = properties["detail"]
+                                geometry = feature["geometry"]
+                                longitude = geometry["coordinates"][0]
+                                latitude = geometry["coordinates"][1]
+                                
+                                outfile.write("{};{};{};{};{};{}\n".format(title, event_time, mag, latitude, longitude, url)) # Use ";" as the deliminator
+
+                            break
+
+                        except Exception as e:
+                            print("Error: {}\nRetrying in 1 minute\n".format(e))
+                            time.sleep(60)
+                            
+            # Close the file
+            outfile.close()
+
     def getLast24Hours(self):
         """
         Get the API results for the last 2 days
@@ -126,7 +185,6 @@ class EarthquakeApi:
         
         data = [] # list to store the objects
         response = requests.get("{}query?format=geojson&starttime={}&endtime={}".format(self.url, (date.today() - timedelta(days = 1)), date.today())).json()
-        print("{}query?format=geojson&starttime={}&endtime={}".format(self.url, (date.today() - timedelta(days = 1)), date.today()))
 
         for feature in response["features"]:
             properties = feature["properties"]
@@ -142,3 +200,6 @@ class EarthquakeApi:
             data.append(jsonExport)
 
         return data
+
+x = EarthquakeApi("https://earthquake.usgs.gov/fdsnws/event/1/")
+x.getParsedDataTXT("1568-01", "1573-01", "API/earthquakeData")
--- API/test.py
+++ API/test.py
@@ -8,34 +8,46 @@ class test_Api(unittest.TestCase):
     def setUp(self):
          self.e = EarthquakeApi('https://earthquake.usgs.gov/fdsnws/event/1/')
 
-    def test_getAllData(self):
+    def test_getAllJSONData(self):
 
-        self.e.getAllData("1568-01", "1568-01", "testdata")
+        self.e.getAllDataJSON("1568-01", "1568-01", "API/testdata")
 
-        with open("testdata/getAllDataTest.json", "r") as f1:
+        with open("API/testdata/getAllDataTest.json", "r") as f1:
             data1 = json.load(f1)
         
-        with open("testdata/1568_01.json", "r") as f2:
+        with open("API/testdata/1568_01.json", "r") as f2:
             data2 = json.load(f2)
 
         self.assertEqual(data1["features"][0]["id"], data2["features"][0]["id"])
-        os.remove("testdata/1568_01.json")
+        os.remove("API/testdata/1568_01.json")
        
 
-    def test_getParsedData(self):
-        self.e.getParsedData("1568-01", "1568-01", "testdata")
+    def test_getParsedDataJSON(self):
+        self.e.getParsedDataJSON("1568-01", "1568-01", "API/testdata")
 
-        with open("testdata/getParsedDataTest.json", "r") as f:
+        with open("API/testdata/getParsedDataTest.json", "r") as f:
             data1 = json.load(f)
-        with open("testdata/getParsedDataTest.json", "r") as f:
+        with open("API/testdata/getParsedDataTest.json", "r") as f:
             data2 = json.load(f)
         
         self.assertEqual(data1[0]["title"], data2[0]["title"])
-        os.remove("testdata/earthquakesParsed.json")
+        os.remove("API/testdata/earthquakesParsed.json")
 
-    def test_fetLast24Hours(self):
+
+    def test_getParsedDataTXT(self):
+        self.e.getParsedDataTXT("1568-01", "1568-01", "API/testdata")
+
+        with open("API/testdata/getParsedDataTest.txt") as f:
+            data1 = f.read()
+        with open("API/testdata/earthquakesParsed.txt") as f:
+            data2 = f.read()
+
+        self.assertEqual(data1, data2)
+        os.remove("API/testdata/earthquakesParsed.txt")
+
+
+    def test_getLast24Hours(self):
         self.assertTrue(self.e.getLast24Hours())
-
 
 
 unittest.main()
