2d65ca8c33dce9ecd9b83a72587790ae20fc8190
Anthony <anthonyargatoff@gmail.com>
2024-03-17 20:06:19 -0700

    Code clarity: Added comments about the methods and parameters

--- src/notification/Notification.py
+++ src/notification/Notification.py
@@ -1,7 +1,16 @@
 class Notification:
+    """
+    Gets custom user notification attributes from the database and compares them to newly acquired data from the API.
+    """    
 
     def __init__(self, userId, dbAttributes: str):
+        """
+        Initializes the object and parses the string.
 
+        Args:
+            userId (int): Stores the user id so that a notification may be sent to them
+            dbAttributes (str): Attribute string from database in format "magnitude:4.56-7.56;area:24.54,46.34,50;". The magnitude attribute will accept a float to a float of magnitude rating a user is interested in. The area is latitude, longitude, and radius.
+        """    
         # Define all the variables, then check if they exist
         self.minMagnitude = None
         self.maxMagnitude = None
@@ -25,8 +34,21 @@ class Notification:
                 location = areaSplit[1].split(",")
                 self.latitude, self.longitude, self.radius = float(location[0]), float(location[1]), float(location[2])
 
-    def compareToDatabase(self, magnitude, latitude, longitude, radius) -> bool:
+    def compareToDatabase(self, magnitude: float, latitude: float, longitude: float) -> bool:
+        """
+        Will get the new results from the database and make a comparison to determine if a notification is to be sent.
+
+
+        Args:
+            magnitude (float): The magnitude of the event
+            latitude (float): Latitude of the event
+            longitude (float): Longitude of the event
+
+        Returns:
+            bool: Returns true if the new data matches the users notification, else will return false.
+        """            
         if (magnitude < self.minMagnitude or magnitude > self.maxMagnitude):
              return False
+        # elif distance calculation will go here
         else:
             return True
