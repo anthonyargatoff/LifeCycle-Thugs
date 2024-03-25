3f95d61221092eb679057bd32f8937575d476855
RyanPybus <ryanpybus8596@gmail.com>
2024-03-25 10:33:14 -0700

    Updated tests to include admin field

--- SQLite/DBManager.py
+++ SQLite/DBManager.py
@@ -146,7 +146,7 @@ class DBUser():
             con.close();
             if (len(list)) != 1: 
                 return False;
-            if (list[0][0] == email and list[0][1] == password and list[0][1] == 1):
+            if (list[0][0] == email and list[0][1] == password and list[0][2] == 1):
                 return True;
             else: return False;
         except Exception as e:
