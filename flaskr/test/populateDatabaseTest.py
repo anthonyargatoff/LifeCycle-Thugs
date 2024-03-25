6696f0da192a669f27cd73df04dc90e23167f3dc
Anthony <anthonyargatoff@gmail.com>
2024-03-25 10:46:54 -0700

    Update correct fields for tests

--- flaskr/test/populateDatabaseTest.py
+++ flaskr/test/populateDatabaseTest.py
@@ -14,7 +14,7 @@ class Test(unittest.TestCase):
 
     # Test the populateDatabase.py adding from text file works as expected
     def test_InsertFromTextFile(self):
-        self.cursor.execute('CREATE TABLE earthquakesTest (id INTEGER PRIMARY KEY AUTOINCREMENT,title VARCHAR(50),time Integer,magnitude decimal (6, 4),latitude decimal (9, 6),longitude decimal (9, 6), depth decimal (10,6), url varchar(255));')
+        self.cursor.execute('CREATE TABLE earthquakesTest (id INTEGER PRIMARY KEY AUTOINCREMENT,title VARCHAR(50),eventTime datetime,magnitude decimal (6, 4),latitude decimal (9, 6),longitude decimal (9, 6), depth decimal (10,6), url varchar(255));')
         x = populateDb(testDb, testLocation + 'multipleLinesTest.txt')
         x.populateAllTxtData("earthquakesTest")
         result = self.cursor.execute("SELECT latitude FROM earthquakesTest WHERE id=4")
